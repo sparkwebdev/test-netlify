b7a236df9df56534131fde1dcc259f02874cbe6d
Steven Park <info@stevenpark.co.uk>
2020-02-27 15:18:44 +0000

    fix admin config

--- .eleventy.js
+++ .eleventy.js
@@ -11,25 +11,4 @@ module.exports = function(eleventyConfig) {
     });
     // Find and copy any `jpg` files, maintaining directory structure.
     eleventyConfig.addPassthroughCopy("admin/**/*.yml");
-
-    return {
-      templateFormats: ["md", "njk", "html", "liquid"],
-  
-      // If your site lives in a different subdirectory, change this.
-      // Leading or trailing slashes are all normalized away, so donâ€™t worry about it.
-      // If you donâ€™t have a subdirectory, use "" or "/" (they do the same thing)
-      // This is only used for URLs (it does not affect your file structure)
-      pathPrefix: "/",
-  
-      markdownTemplateEngine: "liquid",
-      htmlTemplateEngine: "njk",
-      dataTemplateEngine: "njk",
-      passthroughFileCopy: true,
-      dir: {
-        input: ".",
-        includes: "_includes",
-        data: "_data",
-        output: "_site"
-      }
-    };
 };
