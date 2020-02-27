9d52ce9fcfb0dc1d06cc24b045702d287ac282be
Steven Park <info@stevenpark.co.uk>
2020-02-27 14:27:43 +0000

    config setup tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("machineDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+    });
     // Find and copy any `jpg` files, maintaining directory structure.
-    eleventyConfig.addPassthroughCopy("**/*.yml");
+    eleventyConfig.addPassthroughCopy("*.yml");
 };
