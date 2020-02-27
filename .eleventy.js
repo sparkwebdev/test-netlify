8d7c0d06e73e99de5ac84a0a07fb86537def13fe
Steven Park <info@stevenpark.co.uk>
2020-02-27 14:35:41 +0000

    fix date filter errors

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+    // Date formatting (human readable)
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    });
+    // Date formatting (machine readable)
     eleventyConfig.addFilter("machineDate", dateObj => {
         return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
     });
